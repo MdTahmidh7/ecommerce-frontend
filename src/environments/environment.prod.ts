18dcf5d712374b672670234be5e6f8c792efdb10
Tahmid <mdtahmidh7@gamil.com>
2025-09-24 16:43:46 +0600

    Add OTP expiry time configuration to environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://20.55.108.20:8080/api',
-  imageBaseUrl: 'http://20.55.108.20:9000/ecommerce-products'
+  imageBaseUrl: 'http://20.55.108.20:9000/ecommerce-products',
+  otpExpiryTimeInMin: 5 // OTP expiry time in minutes
 };
