2dcfb407157e00df86153a5f2ba3b47f32bccfe9
Tahmid <abdullah.tahmid@bracits.com>
2025-10-22 00:56:40 +0600

    update production environment API and image base URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://20.55.108.20:8080/api',
-  imageBaseUrl: 'http://20.55.108.20:9000/ecommerce-products',
+  apiBaseUrl: 'http://82.180.145.36:8080/api',
+  imageBaseUrl: 'http://82.180.145.36/:9000/ecommerce-products',
   otpExpiryTimeInMin: 5, // OTP expiry time in minutes
   shippingCost: 120 // Flat shipping cost
 };
