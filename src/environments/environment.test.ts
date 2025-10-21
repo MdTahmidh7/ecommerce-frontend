01cb3adc24c7e2a8d0c095d6131b943e852c4140
Tahmid <abdullah.tahmid@bracits.com>
2025-10-21 23:21:25 +0600

    Add test environment configuration and update deployment workflow for tag-based environment handling

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://20.55.108.20:8080/api',
+  imageBaseUrl: 'http://20.55.108.20:9000/ecommerce-products',
+  otpExpiryTimeInMin: 2, // OTP expiry time in minutes
+  shippingCost: 120 // Flat shipping cost
+};
